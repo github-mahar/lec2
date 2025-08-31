071d2bfdbdac0a809b2bb9ab5f3422e32e60ea3e
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2025-08-31 23:40:48 +0500

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function Navbar({ logo }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className="bg-white shadow">
+      <nav className="flex justify-between items-center w-[92%] mx-auto relative">
+        {/* Logo */}
+        <div>
+          <img className="w-16 cursor-pointer" src={logo} alt="logo" />
+        </div>
+
+        {/* Nav Links */}
+        <ul
+          className={`flex flex-col md:flex-row md:items-center md:gap-[4vw] gap-8
+            absolute md:static left-0 w-full md:w-auto bg-white md:bg-transparent
+            overflow-hidden transition-all duration-500 ease-in-out px-16
+            ${open ? "max-h-96 py-6 top-16" : "max-h-0 top-16"}
+            md:max-h-none md:py-0 md:flex`}
+        >
+          <li>
+            <Link to="/" className="hover:text-gray-500">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-gray-500">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-gray-500">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/products" className="hover:text-gray-500">
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link to="/features" className="hover:text-gray-500">
+              Features
+            </Link>
+          </li>
+        </ul>
+
+        {/* Buttons + Hamburger */}
+        <div className="flex items-center gap-6">
+            <Link to="/cart">
+          <button className="bg-gray-200 px-5 py-2 rounded-full hover:bg-gray-300">
+              <i className="fa-solid fa-cart-shopping"></i>
+          </button>
+            </Link>
+          <button className="bg-[#ac61ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
+            <Link to="/login">Login</Link>
+          </button>
+
+          {/* Hamburger Icon → hidden on md+ */}
+          <i
+            onClick={() => setOpen(!open)}
+            className={`fa-solid ${open ? "fa-xmark" : "fa-bars"} text-3xl cursor-pointer md:!hidden`}
+          ></i>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/productDetails.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import Navbar from './navbar';
+
+export default function ProductDetails() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, [id]);
+  if (!product) {
+    return (
+<div className="fixed inset-0 flex items-center justify-center bg-black/20 z-50">
+  <div className="relative">
+    <div className="relative w-32 h-32">
+      <div
+        className="absolute w-full h-full rounded-full border-[3px] border-gray-100/10 border-r-[#0ff] border-b-[#0ff] animate-spin"
+        style={{ animationDuration: "3s" }}
+      ></div>
+
+      <div
+        className="absolute w-full h-full rounded-full border-[3px] border-gray-100/10 border-t-[#0ff] animate-spin"
+        style={{ animationDuration: "2s", animationDirection: "reverse" }}
+      ></div>
+    </div>
+
+    <div className="absolute inset-0 bg-gradient-to-tr from-[#0ff]/10 via-transparent to-[#0ff]/5 animate-pulse rounded-full blur-sm"></div>
+  </div>
+</div>
+
+
+    );
+  }
+  return (
+    <>
+    <div className="flex items-center justify-center md:mt-20 flex-wrap md:flex-nowrap">
+      <div className="border-r-4 border-gray-600 border-t-4 p-4 rounded-lg shadow-lg m-10">
+        <img src={product.image} alt={product.title} className='w-80 object-contain rounded-lg' />
+      </div>
+      <div className='max-w-xl'>
+        <h1 className='text-2xl font-bold mt-4'>{product.title}</h1>
+        <p className='text-gray-600 mt-2'>{product.description}</p>
+        <p className='text-lg font-semibold mt-4'>${product.price}</p>
+        <button className='mt-4 px-4 py-2 text-gray-700 rounded-lg'>Add to Cart</button>
+        <button className='mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded-lg m-3'>Buy Now</button>
+      </div>
+
+    </div>
+    </>
+  )
+}
